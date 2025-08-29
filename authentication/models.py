e072520bf64ce95f4531749b49a48495ebdbda9e
Guen Nhat King <guennhatking@gmail.com>
2025-08-29 12:45:22 +0700

    Add models for authentication

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class UserAccount(models.Model):
+    username = models.CharField(max_length=64, unique=True)
+    salt = models.BinaryField(max_length=32)
+    hashed = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.username

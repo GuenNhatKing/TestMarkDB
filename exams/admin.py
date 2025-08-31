3fcfad50556b4c06f267e979313c7fadb8ddbfef
Guen Nhat King <guennhatking@gmail.com>
2025-08-31 07:48:36 +0700

    Remove exams/

--- exams/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-# Register your models here.
-admin.site.register(Exam)
-admin.site.register(ExamAnswer)
-admin.site.register(Examinee)
-admin.site.register(ExamineeList)
-admin.site.register(ExamineePaper)
-admin.site.register(ExamPaper)

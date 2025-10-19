580add44ff9b9c7c0bcc6fe9d99f10b96d419141
Guen Nhat King <guennhatking@gmail.com>
2025-10-19 23:50:11 +0700

    fix prod

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -8,4 +8,21 @@ python ./manage.py collectstatic --noinput
 python ./manage.py makemigrations --noinput || true
 python ./manage.py migrate --noinput
 
-exec "$@"
+if [ "$PROD" = "true" ]; then
+  echo "Production mode detected."
+
+  if [ "$RUN_APP" = "true" ]; then
+    echo "Starting uwsgi server..."
+    exec uwsgi --ini ./uwsgi.ini
+  elif [ "$RUN_CELERY" = "true" ]; then
+    echo "Starting Celery worker..."
+    exec celery -A root worker --loglevel=INFO --pool=threads
+  else
+    echo "No RUN_APP or RUN_CELERY environment variable found."
+    exit 1
+  fi
+
+else
+  echo "Development mode detected. Running custom command..."
+  exec "$@"
+fi
